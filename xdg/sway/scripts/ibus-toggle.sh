c3e1393de60527c572672b4fcb080193512b9e15
Shou Ya <shouya@users.noreply.github.com>
2022-01-06 16:11:34 +0800

    sway: Alt+shift+space to toggle input method

---
+++ xdg/sway/scripts/ibus-toggle.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if ! pgrep 'ibus-daemon' 2>/dev/null; then
+  ibus-daemon -drx
+  ibus engine chewing
+  notify-send "Current IBus engine: chewing"
+else
+  case "$(ibus engine)" in
+    "chewing")
+      ibus engine xkb:us::eng
+      notify-send "Current IBus engine: xkb:us::eng"
+      ;;
+
+    "xkb:us::eng")
+      ibus engine chewing
+      notify-send "Current IBus engine: chewing"
+      ;;
+
+    *)
+      ibus engine xkb:us::eng
+      notify-send "Invalid IBus engine, switching to xkb:us::eng"
+      ;;
+  esac
+fi
