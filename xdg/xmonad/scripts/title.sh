36e8e3b3502d87955786e5f61a51e682e64c6f35
Shou Ya <shouya@users.noreply.github.com>
2022-03-23 23:21:09 +0800

    [xmonad] add status bar script to show current event

---
+++ xdg/xmonad/scripts/title.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if pgrep zoom &>/dev/null; then
+	echo "****"
+	exit
+fi
+
+export PATH=/home/shou/.asdf/shims:/home/shou/.asdf/bin:/home/shou/.cargo/bin:/home/shou/.local/bin:/home/shou/.shell/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+khal list now | head -n2 | tail -n1
+
+
