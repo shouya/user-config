518e999d6bbca9006072fa64d4bb47eb0515ebd6
Shou Ya <shouya@users.noreply.github.com>
2022-04-03 18:42:27 +0800

    [xmonad] allow manual invocation on new keyboard

--- xdg/xmonad/scripts/new-keyboard.sh
+++ xdg/xmonad/scripts/new-keyboard.sh
@@ -5,13 +5,24 @@
 # $0 event-type device-id device-type device-name
 #
 
+
+echo "$@"
+
 event_type="$1"
 device_id="$2"
 device_type="$3"
 device_name="$4"
 
-if [[ "$device_type" = *Keyboard* && "$event_type" = XIDeviceEnabled ]]; then
+setup_keyboard() {
   echo "New keyboard detected; configuring..."
   setxkbmap -option ctrl:nocaps,altwin:swap_lalt_lwin
   xset r rate 200 45
+}
+
+if [[ "$#" = 0 ]]; then
+  setup_keyboard
+fi
+
+if [[ "$device_type" = *Keyboard* && "$event_type" = XIDeviceEnabled ]]; then
+  setup_keyboard
 fi
