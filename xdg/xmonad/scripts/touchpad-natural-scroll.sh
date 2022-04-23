1177e4e06dcdfc5bdc86a98cfd17601ebb2a0960
Shou Ya <shouya@users.noreply.github.com>
2022-04-23 22:04:39 +0800

    [xmonad] fix touchpad setting

--- xdg/xmonad/scripts/touchpad-natural-scroll.sh
+++ xdg/xmonad/scripts/touchpad-natural-scroll.sh
@@ -5,7 +5,7 @@
 # - xinput list
 # - xinput list-prop "$dev_id"
 
-dev="$SynPS/2 Synaptics TouchPad"
+dev="SynPS/2 Synaptics TouchPad"
 
 xinput set-prop "$dev" 'libinput Tapping Enabled' 1
 xinput set-prop "$dev" 'libinput Natural Scrolling Enabled' 1
