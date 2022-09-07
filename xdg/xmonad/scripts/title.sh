ddc11c887f423e30f626a8ecf02763c5c478ab73
Shou Ya <shouya@users.noreply.github.com>
2022-09-07 22:17:41 +0800

    [polybar] show a message when day finishes

--- xdg/xmonad/scripts/title.sh
+++ xdg/xmonad/scripts/title.sh
@@ -6,6 +6,10 @@ if pgrep zoom &>/dev/null; then
 fi
 
 export PATH=/home/shou/.asdf/shims:/home/shou/.asdf/bin:/home/shou/.cargo/bin:/home/shou/.local/bin:/home/shou/.shell/bin:/usr/local/bin:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin
+
 khal list now | head -n2 | tail -n1
+
+# if no lines are printed before this, show a message
+echo 'Done for today!'
 
 
