26502cdad5c3f0f784a42754c922a2cda6aff9b8
Shou Ya <shouya@users.noreply.github.com>
2022-09-28 15:02:03 +0800

    [xmonad] make slop border more conspicuous

--- xdg/xmonad/xmonad.hs
+++ xdg/xmonad/xmonad.hs
@@ -177,7 +177,7 @@ myKeybinding conf = conf
           [ ("C-M-f", withFocused toggleFloat)
           , ("M-<Tab>", cycleRecentWS [xK_Super_L] xK_Tab (xK_Shift_L .|. xK_Tab))
           , ("<Print>", spawn "flameshot gui")
-          , ("C-<Print>", spawn "flameshot gui --region $(slop)")
+          , ("C-<Print>", spawn "flameshot gui --region $(slop -b 5 -p -5)")
           , ("<XF86AudioLowerVolume>", lowerVolume 5 >> playVolume)
           , ("<XF86AudioRaiseVolume>", raiseVolume 5 >> playVolume)
           , ("<XF86AudioMute>", toggleMute >> pure ())
