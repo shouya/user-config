ac429c943e120192ce6c32c34121e0269d4eb972
Shou Ya <shouya@users.noreply.github.com>
2022-09-28 13:12:20 +0800

    [xmonad] make C-<Print> flameshot capture a window

--- xdg/xmonad/xmonad.hs
+++ xdg/xmonad/xmonad.hs
@@ -175,7 +175,7 @@ myKeybinding conf = conf
           [ ("C-M-f", withFocused toggleFloat)
           , ("M-<Tab>", cycleRecentWS [xK_Super_L] xK_Tab (xK_Shift_L .|. xK_Tab))
           , ("<Print>", spawn "flameshot gui")
-          , ("C-<Print>", spawn "flameshot full")
+          , ("C-<Print>", spawn "flameshot gui --region $(slop)")
           , ("<XF86AudioLowerVolume>", lowerVolume 5 >> playVolume)
           , ("<XF86AudioRaiseVolume>", raiseVolume 5 >> playVolume)
           , ("<XF86AudioMute>", toggleMute >> pure ())
