169382f85236ea889e0a92ff7ba75541d8a4fe93
Shou Ya <shouya@users.noreply.github.com>
2023-01-20 20:03:01 +0900

    [xmonad] update dev name in touchpad-natural-scroll.sh

--- xdg/xmonad/scripts/touchpad-natural-scroll.sh
+++ xdg/xmonad/scripts/touchpad-natural-scroll.sh
@@ -5,7 +5,7 @@
 # - xinput list
 # - xinput list-prop "$dev_id"
 
-dev="SynPS/2 Synaptics TouchPad"
+dev="Synaptics TM3625-010"
 
 xinput set-prop "$dev" 'libinput Tapping Enabled' 1
 xinput set-prop "$dev" 'libinput Natural Scrolling Enabled' 1
