e163223d1f5ee13014ddef14b1e5e98da00ae009
Shou Ya <shouya@users.noreply.github.com>
2023-01-22 22:55:53 +0900

    [eww] do not show battery alert when not charging
    unless the battery is discharging, it's probably fine.

--- xdg/eww/scripts/battery.sh
+++ xdg/eww/scripts/battery.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-acpi -b acpi -b | awk -F'(: |, )' '{gsub("%", ""); print "{\"name\":\""$1"\",\"status\":\""$2"\",\"capacity\":"$3",\"rate\":\""$4"\"}"}' | jq --slurp --compact-output
+acpi -b acpi -b | \
+  awk -F'(: |, )' '{gsub("%", ""); print "{\"name\":\""$1"\",\"status\":\""$2"\",\"capacity\":"$3",\"rate\":\""$4"\"}"}' | \
+  jq --slurp --compact-output
