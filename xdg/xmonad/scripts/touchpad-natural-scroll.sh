d9db5e17ac51e42f0b20f4ee06669b2b6fbee723
Shou Ya <shouya@users.noreply.github.com>
2023-07-11 22:47:27 +0900

    [xmonad] update trackpad device name

--- xdg/xmonad/scripts/touchpad-natural-scroll.sh
+++ xdg/xmonad/scripts/touchpad-natural-scroll.sh
@@ -5,7 +5,8 @@
 # - xinput list
 # - xinput list-prop "$dev_id"
 
-dev="Synaptics TM3625-010"
+# dev="Synaptics TM3625-010"
+dev="SYNA8013:00 06CB:CE69 Touchpad"
 
 xinput set-prop "$dev" 'libinput Tapping Enabled' 1
 xinput set-prop "$dev" 'libinput Natural Scrolling Enabled' 1
