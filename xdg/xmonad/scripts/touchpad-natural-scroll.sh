ae0fb6d9a89972357b50e7cc6a072c3506eaedd1
Shou Ya <shouya@users.noreply.github.com>
2023-07-25 16:31:51 +0900

    [xmonad] map touchscreen input to built-in lcd

--- xdg/xmonad/scripts/touchpad-natural-scroll.sh
+++ xdg/xmonad/scripts/touchpad-natural-scroll.sh
@@ -10,3 +10,6 @@ dev="SYNA8013:00 06CB:CE69 Touchpad"
 
 xinput set-prop "$dev" 'libinput Tapping Enabled' 1
 xinput set-prop "$dev" 'libinput Natural Scrolling Enabled' 1
+
+touchscreen='Wacom Pen and multitouch sensor Finger touch'
+xinput map-to-output "$touchscreen" eDP-1
