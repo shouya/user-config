8c337ee595466545ba9d0a7f471db7b3ad5fa093
Shou Ya <shouya@users.noreply.github.com>
2023-11-28 01:09:29 +0900

    [fish][starship] Add incognito mode

--- xdg/fish/functions/incognito.fish
+++ xdg/fish/functions/incognito.fish
@@ -1,3 +1,13 @@
 function incognito
-    fish --private
+    if not set -q fish_private_mode
+        set -g fish_private_mode 1
+        set -gx incognito_mode I
+        echo "Incognito mode enabled"
+    else
+        # -e: erase
+        set -ge fish_private_mode
+        # --global --unexport
+        set -gu incognito_mode
+        echo "Incognito mode disabled"
+    end
 end
