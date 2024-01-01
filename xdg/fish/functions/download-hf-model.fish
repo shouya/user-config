91f54e772fefda3dbe604f055a2d51a53fa8da1c
Shou Ya <shouya@users.noreply.github.com>
2024-01-01 18:36:47 +0900

    [fish] add a download script

---
+++ xdg/fish/functions/download-hf-model.fish
@@ -0,0 +1,10 @@
+# input a url like
+# https://huggingface.co/TheBloke/Sakura-SOLAR-Instruct-GGUF/resolve/main/sakura-solar-instruct.Q4_K_S.gguf?download=true
+# to download the llm model to the designated location
+
+function download-hf-model --arg url
+    set -l filename (echo $url | sed -e 's/.*\///' -e 's/\?.*//')
+    er-x-direct-client add $(hostname -I)
+    a2c -o ~/models/llama.cpp/$filename "$url"
+    er-x-direct-client reset
+end
