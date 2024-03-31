719c88c989d01e75618540ebf4b7cdf221544881
shouya <526598+shouya@users.noreply.github.com>
2024-04-01 00:01:38 +0900

    [eww] suppress error when workspace has no active window

--- xdg/eww/scripts/active-window-title.py
+++ xdg/eww/scripts/active-window-title.py
@@ -1,19 +1,30 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 
 import os
 import subprocess
 
+
 def get_xmonad_title():
     prop = "_XMONAD_TITLE_LOG"
-    process = subprocess.Popen(['xprop', '-spy', '-notype', '-root', '-f', prop, '8t', '!$0\\n', prop], stdout=subprocess.PIPE, text=True, bufsize=0)
+    process = subprocess.Popen(
+        ["xprop", "-spy", "-notype", "-root", "-f", prop, "8t", "!$0\\n", prop],
+        stdout=subprocess.PIPE,
+        text=True,
+        bufsize=0,
+    )
     while True:
         output = process.stdout.readline().strip()
-        if output:
-            title = output.split('!')[1].strip()
-            title = title.strip('\"')
-            if title:
-                print(title, flush=True)
+        if not output:
+            continue
+        title = output.split("!")[1].strip()
+        title = title.strip('"')
+        if title == "<field not available>":
+            print("", flush=True)
+            continue
+
+        if title:
+            print(title, flush=True)
+
 
 if __name__ == "__main__":
     get_xmonad_title()
-
