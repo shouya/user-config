3af727e75b9421f8e9323f1713eca7147735c241
shouya <526598+shouya@users.noreply.github.com>
2025-01-31 01:05:35 +0900

    disable audible bells

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -85,6 +85,13 @@
   services.pipewire = {
     enable = true;
     pulse.enable = true;
+    extraConfig.pipewire = {
+      "99-disable-bell" = {
+        "context.properties"= {
+          "module.x11.bell" = false;
+        };
+      };
+    };
   };
   services.udisks2.enable = true; # required by udiskie
 
